0a6cf94ec337750f3ea67cce74fd8c82c9601e77
catatonic2aucourant <mohsinjackspiro@gmail.com>
2018-03-02 10:25:35 +0530

    Added hyperopt

---
+++ RocAucEvaluation.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar  2 09:08:50 2018
+
+@author: mohsin
+"""
+
+from sklearn.metrics import roc_auc_score
+from keras.callbacks import Callback
+class RocAucEvaluation(Callback):
+    def __init__(self, validation_data=(), interval=1):
+        super(Callback, self).__init__()
+
+        self.interval = interval
+        self.X_val, self.y_val = validation_data
+
+    def on_epoch_end(self, epoch, logs={}):
+        if epoch % self.interval == 0:
+            y_pred = self.model.predict(self.X_val, verbose=0)
+            score = roc_auc_score(self.y_val, y_pred)
+            print("\n ROC-AUC - epoch: %d - score: %.6f \n" % (epoch+1, score))
